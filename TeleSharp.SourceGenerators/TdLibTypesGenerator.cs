5ea129cd0edd82d67498f254d0760dfd1435c654
roxxel <64275777+roxxel@users.noreply.github.com>
2022-07-12 22:02:23 +0300

    update

---
+++ TeleSharp.SourceGenerators/TdLibTypesGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TdLib.TdApi;
+
+namespace TeleSharp.SourceGenerators
+{
+    [Generator]
+    public sealed partial class TdLibTypesGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+}
